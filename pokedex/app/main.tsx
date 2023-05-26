17682baa99b9a36f17b3407668953ce2b0888e4b
junito <bellamy992@gmail.com>
2023-05-26 23:03:20 +0200

    prueba de enlazar

--- pokedex/app/main.tsx
+++ pokedex/app/main.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import PokemonList from "../app/page";
+import { BrowserRouter as Router, Routes, Route,Link } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import PokemonList from "../app/page";
+import Pokedex from "../app/pokedex";
 
 const HomePage = () => {
   return (
     <ChakraProvider>
-      
-        <PokemonList />
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<PokemonList />} />
+          <Route path="/pokedex" element={<Pokedex />} />
+        </Routes>
+      </Router>
     </ChakraProvider>
   );
 };
 
 export default HomePage;
-
-
